fac1b1a8c85b88a88ef69eb9e0138b8a6d311300
zhaiyuanyuan <18239065939@163.com>
2017-10-20 09:16:57 +0800

    提交路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,15 @@ import Router from 'vue-router'
 import homepage from '@/components/homepage'
 import invest from '@/components/invest'
 import personal from '@/components/personal'
-import setTransactionPassword from '@/components/setTransactionPassword'
+import setPayPsd from '@/components/setPayPsd'
 import certification from '@/components/certification'
+import accountSet from '@/components/accountSet'
+import modifyLoginPsdB from '@/components/modifyLoginPsdB'
+import modifyLoginPsdA from '@/components/modifyLoginPsdA'
+import modifyPayPsdA from '@/components/modifyPayPsdA'
+import modifyPayPsdB from '@/components/modifyPayPsdB'
+import cashCommission from '@/components/cashCommission'
+
 
 
 
@@ -13,7 +20,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/homepage',
       name: 'homepage',
       component: homepage
     },
@@ -28,14 +35,45 @@ export default new Router({
       component: personal
     },
     {
-      path: '/setTransactionPassword',
-      name: 'setTransactionPassword',
-      component: setTransactionPassword
+      path: '/setPayPsd',
+      name: 'setPayPsd',
+      component: setPayPsd
     },
     {
       path: '/certification',
       name: 'certification',
       component: certification
+    },
+    {
+      path: '/accountSet',
+      name: 'accountSet',
+      component: accountSet
+    },
+    {
+      path: '/modifyLoginPsdB',
+      name: 'modifyLoginPsdB',
+      component: modifyLoginPsdB
+    },
+    {
+      path: '/modifyLoginPsdA',
+      name: 'modifyLoginPsdA',
+      component: modifyLoginPsdA
+    },
+    {
+      path: '/modifyPayPsdA',
+      name: 'modifyPayPsdA',
+      component: modifyPayPsdA
+    },
+    {
+      path: '/modifyPayPsdB',
+      name: 'modifyPayPsdB',
+      component: modifyPayPsdB
+    },
+    {
+      path: '/cashCommission',
+      name: 'cashCommission',
+      component: cashCommission
     }
+    
   ]
 })
